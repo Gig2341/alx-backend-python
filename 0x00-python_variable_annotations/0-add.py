70504fe8c62afbcff75de950bb95ddf523f0ca39
Gig2341 <danmata2341@gmail.com>
2023-12-07 21:01:42 +0000

    tasks 1 and 2

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+a function to find sum
+"""
+
+def add(a: float, b: float) -> float:
+    """Function that takes two floats and return their sum"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+"""
+this module contains a function that concatenates two strings
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenates two strings."""
+    return str1 + str2
