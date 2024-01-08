a9814f11e816b3f3309f6d46c44c46f6dd41ae3d
Gig2341 <danmata2341@gmail.com>
2024-01-08 19:31:57 +0000

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,12 +3,14 @@
 
 
 import unittest
+import requests
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''class to test utils.py'''
+    '''class to test access_nested_map function'''
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -18,6 +20,31 @@ class TestAccessNestedMap(unittest.TestCase):
         '''tests the access_nested_map method'''
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    '''class to test the get_json function'''
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        '''tests the get_json method'''
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('requests.get', return_value=mock_response):
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+            # check that mocked method called once per input
+            mock_response.json.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
